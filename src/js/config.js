b0c100274a0e8ff0e90d390ed9ab9e7ca9c3e940
bolu-da-coder <legendaryboluwatife@gmail.com>
2022-12-01 20:00:05 +0100

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes/`;
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '07b6c991-6a46-437d-ab3e-e94c1c1c1fa1';
+export const MODAL_CLOSE_SEC = 2.5;
